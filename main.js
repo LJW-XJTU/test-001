6be9cabb8d7ad1872fc19eadf451f633cc015511
LJW-XJTU <871098540@qq.com>
2018-05-09 18:12:47 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+
+// function isLeapYear(n)
+// {    
+//     if (n%400==0)    
+//     {        
+//         return true    
+//     }    
+//     if (n%100==0)    
+//     {        
+//         return false    
+//     }    
+//     if (n%4==0)    
+//     {        
+//         return true    
+//     }
+//     return false
+// }
+// console.log("2000"+isLeapYear(2000));
+// console.log("2008"+isLeapYear(2008));
+// console.log("2009"+isLeapYear(2009));
+// console.log("1900"+isLeapYear(1900));
+
+
+const fibonacci = require('./lib/fibonacci')
+
+// var len =20;
+
+// for(; len--; ){
+//     console.log(fibonacci(len))
+// }
+// console.log(fibonacci(1));
+
+// for(var i=1; i<20; i++){
+//     console.log(fibonacci(i))
+// }
+
+console.log(fibonacci(3));
+console.log(fibonacci(1));
---
+++ spec/anagram_spec.js
@@ -0,0 +1,12 @@
+const anagram = require('../lib/anagram')
+
+describe("anagarm", ()=> {
+
+    it ("should return [] when input is empty string", ()=>{
+        expect(anagram()).toEqual([])
+    })
+
+    it ("should return [a] when input is empty string", ()=>{
+        expect(anagram()).toEqual([])
+    })
+})
